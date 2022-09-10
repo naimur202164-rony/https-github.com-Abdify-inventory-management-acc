353a20dfaa0cf57ff83923d4a281f3faedb4f799
MD Naimur Rahman <86639464+naimur202163@users.noreply.github.com>
2022-09-10 16:01:05 +0545

    working on productSchema file

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.use(express.json());
 app.use(cors());
 // working on Schema file
 
+const productSchema=mongoose.Schema({
+  name:{
+    type:String,
+    required:[true,"Please provide  a name for this product"],
+    trim:true
+  }
+})
+
 
 app.get("/", (req, res) => {
   res.send("Route is working! YaY!");
