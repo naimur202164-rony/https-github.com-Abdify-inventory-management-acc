84419c780004a1c4b38b4126f87cfb5074fcce9a
MD Naimur Rahman <86639464+naimur202163@users.noreply.github.com>
2022-09-10 16:09:25 +0545

    schema updated

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const {mongoose}=require('mongoose')
-
+const { mongoose } = require("mongoose");
 
 // Middleware
 app.use(express.json());
 app.use(cors());
 // working on Schema file
 
-const productSchema=mongoose.Schema({
-  name:{
-    type:String,
-    required:[true,"Please provide  a name for this product"],
-    trim:true
-  }
-})
-
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide  a name for this product"],
+    trim: true,
+    unique: [true,"Name Must be unique"],
+    minLength: [3, "Name Must be 3 Characters"],
+    maxLength: [100, "Name is to large"],
+  },
+});
 
 app.get("/", (req, res) => {
   res.send("Route is working! YaY!");
 });
-
 
 module.exports = app;
