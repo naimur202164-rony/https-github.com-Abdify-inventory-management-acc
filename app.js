f2d3d24713266608cc6dd79e9faf66d7b949dab2
naimur <naimur202163@gmail.com>
2022-10-10 09:41:33 +0600

    working with mongoose

--- app.js
+++ app.js
@@ -7,42 +7,6 @@ const { mongoose } = require("mongoose");
 app.use(express.json());
 app.use(cors());
 // working on Schema file
-
-const productSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please provide  a name for this product"],
-    trim: true,
-    unique: [true, "Name Must be unique"],
-    minLength: [3, "Name Must be 3 Characters"],
-    maxLength: [100, "Name is to large"],
-  },
-
-  description: {
-    type: String,
-    required: true,
-  },
-
-  price: {
-    type: Number,
-    required: [true],
-    min: [0, "Price cant be negative "],
-  },
-  unit: {
-    type: String,
-    required: true,
-    enum: {
-      value: ["kg", "liter", "pcs"],
-      message: "unit value cant be {value},must be kg/litter/pcs",
-    },
-  },
-  quantity: {
-    type: Number,
-    required: true,
-    min:[0,"quantity can "]
-  },
-});
-
 // Time:11min 12sec
 
 app.get("/", (req, res) => {
