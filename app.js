55e092d1061686b783e134637d617167c9d162c7
naimur <naimur202163@gmail.com>
2022-10-10 10:46:21 +0600

    validator added

--- app.js
+++ app.js
@@ -11,10 +11,41 @@ const productSchema = mongoose.Schema({
   name: {
     type: String,
     required: [true, "Please Provide a name for this Product"],
+    trim: true,
+    unique: [true, "Name is must be unique"],
+    minLength: [3, "Name Must be at least 3 characters"],
+    maxLength: [100, "Name is too large"],
   },
-  trim: true,
-  unique: true,
-  minLength: [3, "Name Must be at least 3 characters"],
+
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: [0, "price cant be negative"],
+  },
+  unit: {
+    type: String,
+    required: true,
+    enum: ["kg", "liter", "pcs"],
+    message: "unit   value    cant be {value},must be kg/litter/pcs",
+  },
+  qunatity: {
+    type: Number,
+    required: true,
+    min: [0, "qunatitiy can not be negative"],
+    validate: {
+      validator: (value) => {
+        const isInteger = Number.isInteger(value);
+        if (isInteger) {
+          return true;
+        } else return false;
+      },
+      message: "Qunantity must be an integer",
+    },
+  },
 });
 
 // Time:11min 12sec
